994e62e1ddc34f67e374617cd6983ba8a867db51
Alan <alannavaseg_02@hotmail.com>
2020-04-19 13:45:49 -0500

    SE e obtienen los datos de AMazon y se imprimem

--- scrapper.py
+++ scrapper.py
@@ -5,7 +5,21 @@ URL = 'https://www.amazon.com.mx/Apple-A1474-Wi-Fi-Black-Refurbished/dp/B00M4L4N
 
 headers = {"User-agent":"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:75.0) Gecko/20100101 Firefox/75.0"}
 
-page = request.get(URL, headers=headers)
+def check_price():
 
-soup = BeautifulSoup(page.content, 'html.parser')
-print (soup.prettify())
+    page = requests.get(URL, headers=headers)
+
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    title = soup.find(id='productTitle').getText()
+    price = soup.find(id='price').getText()
+
+    converted_price = float(price[13:18])
+    if(converted_price  < 1700):
+        send_mail()
+
+    print ("El precio es: " + price.strip())
+    print ("El precio convertido es: " + converted_price.strip())
+    print ("El titulo del producto es: "+ title.strip())
+def send_mail():
+    
